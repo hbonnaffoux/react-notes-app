b4caae318802bfd9106a6b5ab716818f1eff3e4c
Herve Bonnaffoux <bonnaffouxherve@gmail.com>
2021-11-29 17:35:34 +0100

    added a Add Note component with note style

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
+import { useState } from "react";
+import { nanoid } from 'nanoid';
 import NotesList from "./components/NotesList";
 
 const App = () => {
+  const [notes, setNotes] = useState([
+    {
+    id: nanoid(),
+    text:  "This is my first note",
+    date : "23/04/1994"
+  },
+  {
+    id: nanoid(),
+    text:  "This is my second note",
+    date : "23/04/1994"
+  },
+  {
+    id: nanoid(),
+    text:  "This is my third note",
+    date : "23/04/1994"
+  },
+  {
+    id: nanoid(),
+    text:  "This is my fourth note",
+    date : "23/04/1994"
+  }
+
+]); 
+  
   return (
     <div className="container">
-        <NotesList/>
+        <NotesList notes={notes}/>
     </div>
   );
 };
