eca4c48ba992b778ed2de64b15aa1f82f2d68d66
Herve Bonnaffoux <bonnaffouxherve@gmail.com>
2021-11-30 13:15:56 +0100

    added delete note feature with styling

--- src/App.js
+++ src/App.js
@@ -1,38 +1,56 @@
 import { useState } from "react";
-import { nanoid } from 'nanoid';
+import { nanoid } from "nanoid";
 import NotesList from "./components/NotesList";
 
 const App = () => {
   const [notes, setNotes] = useState([
     {
-    id: nanoid(),
-    text:  "This is my first note",
-    date : "23/04/1994"
-  },
-  {
-    id: nanoid(),
-    text:  "This is my second note",
-    date : "23/04/1994"
-  },
-  {
-    id: nanoid(),
-    text:  "This is my third note",
-    date : "23/04/1994"
-  },
-  {
-    id: nanoid(),
-    text:  "This is my fourth note",
-    date : "23/04/1994"
-  }
+      id: nanoid(),
+      text: "This is my first note",
+      date: "23/04/1994",
+    },
+    {
+      id: nanoid(),
+      text: "This is my second note",
+      date: "23/04/1994",
+    },
+    {
+      id: nanoid(),
+      text: "This is my third note",
+      date: "23/04/1994",
+    },
+    {
+      id: nanoid(),
+      text: "This is my fourth note",
+      date: "23/04/1994",
+    },
+  ]);
 
-]); 
-  
+  const addNote = (text) => {
+    const date = new Date();
+    const newNote = {
+      id: nanoid(),
+      text: text,
+      date: date.toLocaleDateString(),
+    };
+    const newNotes = [...notes, newNote]; // this line creates a new array instead of updating the old one
+    setNotes(newNotes);
+  };
+
+  const deleteNote = (id) => {
+   const newNotes =  notes.filter((note) => note.id !== id);
+    setNotes(newNotes);
+  };
+
   return (
     <div className="container">
-        <NotesList notes={notes}/>
+      <NotesList
+        notes={notes}
+        handleAddNote={addNote}
+        handleDeleteNote={deleteNote}
+      />
     </div>
   );
 };
-
 
 export default App;
--- src/components/Note.js
+++ src/components/Note.js
@@ -1,13 +1,19 @@
-import { MdDeleteForever } from 'react-icons/md';
+import { MdDeleteForever } from "react-icons/md";
 
-const Note = ({ id,text,date}) => {
-  return <div className="note">
+const Note = ({ id, text, date, handleDeleteNote }) => {
+  return (
+    <div className="note">
       <span>{text}</span>
       <div className="note-footer">
-          <small>{date}</small>
-          <MdDeleteForever className='delete-icon' size='1.3em'/>
+        <small>{date}</small>
+        <MdDeleteForever
+          onClick={() => handleDeleteNote(id)}
+          className="delete-icon"
+          size="1.3em"
+        />
       </div>
-  </div>;
+    </div>
+  );
 };
 
 export default Note;
