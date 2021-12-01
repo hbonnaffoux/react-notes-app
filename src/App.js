0ea100962464d9aaa6bfcd1e09e5b45d73b62e52
Herve Bonnaffoux <bonnaffouxherve@gmail.com>
2021-12-01 01:55:20 +0100

    added local storage and dark mode

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { nanoid } from "nanoid";
 import NotesList from "./components/NotesList";
 import Search from "./components/Search";
+import Header from "./components/Header";
 
 const App = () => {
   const [notes, setNotes] = useState([
@@ -27,7 +28,22 @@ const App = () => {
     },
   ]);
 
-const [searchText, setSearchText] = useState('');
+  const [searchText, setSearchText] = useState("");
+
+  const [darkMode, setDarkMode] = useState(false);
+
+  useEffect(() => {
+    const savedNotes = JSON.parse(localStorage.getItem("react-notes-app-data")
+    );
+
+    if (savedNotes) {
+      setNotes(savedNotes);
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("react-notes-app-data", JSON.stringify(notes));
+  }, [notes]);
 
   const addNote = (text) => {
     const date = new Date();
@@ -41,19 +57,23 @@ const [searchText, setSearchText] = useState('');
   };
 
   const deleteNote = (id) => {
-   const newNotes =  notes.filter((note) => note.id !== id);
+    const newNotes = notes.filter((note) => note.id !== id);
     setNotes(newNotes);
   };
 
   return (
-    <div className="container">
-      <Search handleSearchNote={setSearchText}/>
-      <NotesList
-        notes={notes.filter((note) => note.text.toLowerCase().includes(searchText)
+    <div className={`${darkMode && "dark-mode"}`}>
+      <div className="container">
+        <Header handleToggleDarkMode={setDarkMode} />
+        <Search handleSearchNote={setSearchText} />
+        <NotesList
+          notes={notes.filter((note) =>
+            note.text.toLowerCase().includes(searchText)
           )}
-        handleAddNote={addNote}
-        handleDeleteNote={deleteNote}
-      />
+          handleAddNote={addNote}
+          handleDeleteNote={deleteNote}
+        />
+      </div>
     </div>
   );
 };
